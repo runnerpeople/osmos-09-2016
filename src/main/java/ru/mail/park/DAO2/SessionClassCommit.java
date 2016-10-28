89712eb8b53e12826e58bb6b5bf4db39a02f7d87
runnerpeople <gosha8352@gmail.com>
2016-10-28 06:42:03 +0300

    Refactoring with validating

---
+++ src/main/java/ru/mail/park/DAO2/SessionClassCommit.java
@@ -0,0 +1,7 @@
+package ru.mail.park.DAO2;
+
+/**
+ * Created by runnerpeople on 28.10.16.
+ */
+public class SessionClassCommit {
+}
---
+++ src/main/java/ru/mail/park/json/ErrorResponse.java
@@ -0,0 +1,7 @@
+package ru.mail.park.json;
+
+/**
+ * Created by runnerpeople on 28.10.16.
+ */
+public class ErrorResponse {
+}
